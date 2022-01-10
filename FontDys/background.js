9bf57d456f3db658f52bb6735e87d0e0b13a40ce
Vico4 <victoire.stahl@gmail.com>
2022-01-10 16:40:30 +0100

    adapté avec la police Verdana, correction du style du bouton

--- FontDys/background.js
+++ FontDys/background.js
@@ -1,6 +1,6 @@
-let font = 'OpenDys2';
+let font = 'Verdana';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ font });
-  console.log(`Default background font set to ${font}`);
+  console.log(`Default font set to ${font}`);
 });
